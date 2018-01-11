ce874d800774767977df62ff01624312baaaac66
John Nauman <nawman43@gmail.com>
2018-01-11 12:52:00 -0500

    Initial commit

---
+++ My Completed Games/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_Completed_Games.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
